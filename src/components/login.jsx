2b0ae7c4b40c5a66d851e8ad1af1e45dda649eab
Dimitri Mudiangondo <dimitrimudia@gmail.com>
2020-11-05 10:29:31 +0100

    Login form

---
+++ src/components/login.jsx
@@ -0,0 +1,38 @@
+import react, {Component} from 'react';
+import { Grid, Button,Checkbox, Header, Form} from 'semantic-ui-react';
+
+class Login extends Component
+{
+    
+
+     render()
+     {
+            return <div className="ui container">
+                <Header as="h4"> Conexion </Header>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={3}> </Grid.Column>
+                        <Grid.Column width={10}>
+                            <Form>
+                            <Form.Field>
+                                <label>First Name</label>
+                                <input placeholder='First Name' />
+                                </Form.Field>
+                                <Form.Field>
+                                <label>Last Name</label>
+                                <input placeholder='Last Name' />
+                                </Form.Field>
+                                <Form.Field>
+                                <Checkbox label='I agree to the Terms and Conditions' />
+                                </Form.Field>
+                                <Button type='submit'>Submit</Button>
+                            </Form>
+                        </Grid.Column>
+                        <Grid.Column width={3}></Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
+     }
+}
+
+export default Login
